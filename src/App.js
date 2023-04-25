426a30d9ed7f4ecacba89dd7c1e92efd39cd2287
Alexander Drach <alexander.drach.adci@gmail.com>
2023-04-25 15:19:40 +0600

    add markup counter component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-function App() {
+ import Counter from "./components/Counter";
+
+ function App() {
   return (
     <div className="App">
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button type="button">-</button>
+      <span>0</span>
+      <button type="button">+</button>
+    </div>
+  )
+}
+
+export default Counter;
